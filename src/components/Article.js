909fcec7fe9b03c2b84383c4f7da545e05de4f80
Itanael <vn.emploi@laposte.net>
2021-09-04 22:52:18 +0200

    Add LikeOptions

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,9 +1,19 @@
 import { Avatar } from '@material-ui/core'
-import React from 'react'
+import React, { useState } from 'react'
 import "./Article.css"
 import ReactPlayer from 'react-player';
 
 const Article = ({ article }) => {
+
+    const [showBox, setShowBox] = useState(false)
+
+    const handleShowBox = () => {
+        setShowBox(true)
+    }
+
+    const handleClearBox = () => {
+        setShowBox(false)
+    }
 
     return (
         <div className="post">
@@ -20,8 +30,35 @@ const Article = ({ article }) => {
                 {!article.shareImg && article.video ? (<ReactPlayer width={'100%'} url={article.video} />) : (article.shareImg && <img src={article.shareImg} alt="" />)}
             </div>
 
+
             <div className="post__buttons">
-                <div className="inputOption">
+                <div className={`${showBox ? "likeOptions" : "dnone"}`} onMouseEnter={handleShowBox} onMouseLeave={handleClearBox}>
+                    <button className="reactions-menu__reaction">
+                        <img className="reactions-menu__icon" src="/images/like-reaction-icon.svg" alt="" />
+                        <span className="reactions-menu__reaction-description">J'aime</span>
+                    </button>
+                    <button className="reactions-menu__reaction">
+                        <img className="reactions-menu__icon" src="/images/praise-reaction-icon.svg" alt="" />
+                        <span className="reactions-menu__reaction-description">Bravo</span>
+                    </button>
+                    <button className="reactions-menu__reaction">
+                        <img className="reactions-menu__icon" src="/images/appreciation-reaction-icon.svg" alt="" />
+                        <span className="reactions-menu__reaction-description">Soutien</span>
+                    </button>
+                    <button className="reactions-menu__reaction">
+                        <img className="reactions-menu__icon" src="/images/empathy-reaction-icon.svg" alt="" />
+                        <span className="reactions-menu__reaction-description">J'adore</span>
+                    </button>
+                    <button className="reactions-menu__reaction">
+                        <img className="reactions-menu__icon" src="/images/interest-reaction-icon.svg" alt="" />
+                        <span className="reactions-menu__reaction-description">Instructif</span>
+                    </button>
+                    <button className="reactions-menu__reaction">
+                        <img className="reactions-menu__icon" src="/images/maybe-reaction-icon.svg" alt="" />
+                        <span className="reactions-menu__reaction-description">Intéressant</span>
+                    </button>
+                </div>
+                <div className="inputOption" onMouseEnter={handleShowBox} onMouseLeave={handleClearBox}>
                     <img src="/images/like-icon.svg" alt="" />
                     <h4>J'aime</h4>
                 </div>
